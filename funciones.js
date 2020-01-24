946ac008782dd4d5be4b24dac2887d4dfaad072e
8bit-1 <jruizgarcia40@gmail.com>
2020-01-23 21:36:15 -0600

    Mas funciones al archivo funciones.js

--- funciones.js
+++ funciones.js
@@ -1,9 +1,11 @@
 console.log("hello there");
-document.querySelector('#boton').addEventListener("click",getData());
+document.querySelector('#boton').addEventListener("click",function(){
+  getData();
+});
 
 function getData(){
   
-  console.log("soy Datos");
+ // console.log("soy Datos");
   
   const xhttp = new XMLHttpRequest();
 
@@ -16,9 +18,20 @@ function getData(){
     if(this.readyState == 4 && this.status == 200){
 
       //console.log(this.responseText);
-      console.log(JSON.parse(this.responseText));
-      
-    }
+      //console.log(JSON.parse(this.responseText));
+      let datos = JSON.parse(this.responseText);
 
+      let clases = document.querySelector('#clases')
+      clases.innerHTML='';
+
+      for(let item in datos){
+          clases.innerHTML += 
+          `<tr>
+          <th>${item.Codigo}</th>
+          <th>${item.Clase}</th>
+          <th>${item.UV}</th>
+          </tr>`
+      }
+    }
   }
 }
